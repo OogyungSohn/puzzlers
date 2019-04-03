0c4b5103ce2fe5bd6ef6474413acc03aa6d0ef3d
ogs <life2scool@naver.com>
2019-04-03 21:24:07 +0900

    190403 과제

---
+++ src/quiz6_15/MemberService.java
@@ -0,0 +1,12 @@
+package quiz6_15;
+
+public class MemberService {
+    boolean login(String id, String password){
+        if(id=="hong" && password=="12345") return true;
+        else return false;
+    }
+    void logout(String id){
+        if(id=="hong")
+            System.out.println("로그아웃 되었습니다.");
+    }
+}
---
+++ src/quiz6_16/Printer.java
@@ -0,0 +1,18 @@
+package quiz6_16;
+
+public class Printer {
+	void println(int a){
+        System.out.println(a);
+    }
+    void println(boolean a){
+        System.out.println(a);
+    }
+    void println(double a){
+        System.out.println(a);
+    }
+    void println(String a){
+        System.out.println(a);
+    }
+    
+    //17번은 void를 static void로 선언해주기
+}
